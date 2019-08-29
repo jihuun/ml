f6fe2aab067a17339c26c3df6ea8408ce6c5281d
Ji-Hun Kim (with Travis) <jihuun.k@gmail.com>
2019-08-29 11:15:15 +0000

    [skip travis] blog: ml: update gitbook by TravisCI with build number 3
    Signed-off-by: Ji-Hun Kim (with Travis) <jihuun.k@gmail.com>

--- docs/scripts/deploy_gitbook.sh
+++ docs/scripts/deploy_gitbook.sh
@@ -1,9 +1,11 @@
 #! /bin/bash
 
+BLOG_NAME="ml"
 CUR_DIR=$(pwd)
 GITBOOK_DIR="$CUR_DIR/_book"
 NODEMODULS_DIR="$CUR_DIR/node_modules"
 DEPLOY_DIR="$CUR_DIR/docs"
+COMMIT_MSG="[skip travis] blog: $BLOG_NAME: update gitbook by TravisCI with build number $TRAVIS_BUILD_NUMBER"
 
 echo $GITBOOK_DIR $DEPLOY_DIR
 
@@ -12,4 +14,11 @@ cp -R $GITBOOK_DIR/* $DEPLOY_DIR
 rm -r $DEPLOY_DIR/docs
 
 git clean -fx $GITBOOK_DIR
-#git clean -fx $NODEMODULS_DIR
+
+git add .
+git branch
+git checkout master
+git commit -sm "$COMMIT_MSG"
+git branch
+git log -2
+git push "https://${GH_TOKEN}@github.com/jihuun/$BLOG_NAME" master
